8d4f297f17477bc5ed922ac4a1e9c4e2fe374417
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-02-15 15:12:16 -0500

    completed

--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -95,4 +95,77 @@ public class TestPerson {
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void setHeight() {
+        // Given
+        Person person = new Person();
+        double expected = 5;
+
+        // When
+        person.setHeight(expected);
+
+        // Then
+        double actual = person.getHeight();
+        Assert.assertEquals(expected, actual, .000);
+    }
+
+
+    @Test
+    public void setWeight() {
+        // Given
+        Person person = new Person();
+        double expected = 5;
+
+        // When
+        person.setWeight(expected);
+
+        // Then
+        double actual = person.getWeight();
+        Assert.assertEquals(expected, actual, .000);
+    }
+
+    @Test
+    public void setHairColor() {
+        // Given
+        Person person = new Person();
+        String expected = "brown";
+
+        // When
+        person.setHairColor(expected);
+        String actual = person.getHairColor();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setGender() {
+        // Given
+        Person person = new Person();
+        char expected = 'm';
+
+        // When
+        person.setGender(expected);
+        char actual = person.getGender();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setEyeColor() {
+        // Given
+        Person person = new Person();
+        String expected = "Blue";
+
+        // When
+        person.setEyeColor(expected);
+        String actual = person.getEyeColor();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
 }
+
+
